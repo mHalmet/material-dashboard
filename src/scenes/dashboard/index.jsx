73239e96c4895542f68be3db0d4d646550d99b3a
mhalmet <mathias.halmetschlager@gmail.com>
2023-04-06 21:16:35 +0200

    sidebar and teamsite gridview

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,6 +1,15 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+
 const Dashboard = () => {
     return (
-    <div>Dashboard</div>
+    <Box m="20px">
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+            <Header title="DASHBOARD" subtitle="Welcome to your dahsboard"/>
+        </Box>
+    </Box>
+    
+
     );
 }
 
